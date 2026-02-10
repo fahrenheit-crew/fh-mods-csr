60f8f776224a58f9a16ff928c7a05cc7c6970768
evelyntsmg <evelyntsmg@proton.me>
2026-02-10 20:33:09 +0100

    Maint: Update to latest Fahrenheit standards

--- src/module.cs
+++ src/module.cs
@@ -1,112 +1,61 @@
 ﻿global using System;
-global using Fahrenheit.CoreLib.FFX;
+global using Fahrenheit.Core.FFX;
 
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
-using Fahrenheit.CoreLib;
-using Fahrenheit.CoreLib.FFX.Atel;
 
-namespace Fahrenheit.Modules.CSR;
+using Fahrenheit.Core;
+using Fahrenheit.Core.Atel;
 
-public sealed record CSRModuleConfig : FhModuleConfig {
-    [JsonConstructor]
-    public CSRModuleConfig(string configName, bool configEnabled) : base(configName, configEnabled) { }
+namespace Fahrenheit.Mods.CSR;
 
-    public override bool TrySpawnModule([NotNullWhen(true)] out FhModule? fm) {
-        fm = new CSRModule(this);
-        return fm.ModuleState == FhModuleState.InitSuccess;
-    }
-}
-
-public unsafe class CSRModule : FhModule {
-    private readonly CSRModuleConfig _moduleConfig;
+[FhLoad(FhGameId.FFX)]
+public unsafe class CutsceneRemoverModule : FhModule {
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void AtelEventSetUp(uint event_id);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void AtelEventSetUp(u32 event_id);
+    public delegate char* AtelGetEventName(uint event_id);
 
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate char* AtelGetEventName(u32 event_id);
-
-    public static char* get_event_name(u32 event_id)
+    public static char* get_event_name(uint event_id)
         => FhUtil.get_fptr<AtelGetEventName>(0x4796e0)(event_id);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void TOMkpCrossExtMesFontLClutTypeRGBA(
-            u32 p1,
-            u8 *text,
-            f32 x, f32 y,
-            u8 color,
-            u8 p6,
-            u8 tint_r, u8 tint_g, u8 tint_b, u8 tint_a,
-            f32 scale,
-            f32 _);
-
-    public static void draw_text_rgba(
-            u32 p1,
-            u8[] text,
-            f32 x, f32 y,
-            u8 color,
-            u8 p6,
-            u8 tint_r, u8 tint_g, u8 tint_b, u8 tint_a,
-            f32 scale,
-            f32 _) {
-        fixed (u8 *_text = text)
-        FhUtil.get_fptr<TOMkpCrossExtMesFontLClutTypeRGBA>(0x501700)(p1, _text, x, y, color, p6, tint_r, tint_g, tint_b, tint_a, scale, _);
-    }
-
-    public static void draw_text(
-            u32 p1,
-            u8[] text,
-            f32 x, f32 y,
-            u8 color,
-            u8 p6,
-            f32 scale,
-            f32 _) {
-        draw_text_rgba(p1, text, x, y, color, p6, 0x80, 0x80, 0x80, 0x80, scale, _);
-    }
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    private delegate u32 FUN_0086e990(isize signal_info_ptr);
+    private delegate uint FUN_0086e990(nint signal_info_ptr);
 
     private FhMethodHandle<FUN_0086e990> _work_debug;
     private FhMethodHandle<AtelEventSetUp> _csr_event;
 
-    public delegate void CsrEvent(u8* code_ptr);
+    public delegate void CsrEvent(byte* code_ptr);
 
     public static readonly Dictionary<string, CsrEvent> removers = new();
 
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    internal delegate u32 AtelCallTargetExec(AtelBasicWorker* work, AtelStack* stack);
-
-    public CSRModule(CSRModuleConfig moduleConfig) : base(moduleConfig) {
-        _moduleConfig = moduleConfig;
-
-        _work_debug = new(this, "FFX.exe", work_debug, offset: 0x46e990);
-        _csr_event = new(this, "FFX.exe", csr_event, offset: 0x472e90);
-
-        _moduleState  = FhModuleState.InitSuccess;
+    public CutsceneRemoverModule() {
+        _work_debug = new(this, "FFX.exe", 0x46e990, work_debug);
+        _csr_event = new(this, "FFX.exe", 0x472e90, csr_event);
     }
 
-    public override bool FhModuleInit() {
+    public override bool init(FhModContext mod_context, FileStream global_state_file) {
         Removers.init();
 
         return _csr_event.hook()
             && _work_debug.hook();
     }
 
-    private u32 work_debug(isize signal_info_ptr) {
-        u32 ret = _work_debug.orig_fptr(signal_info_ptr);
+    private uint work_debug(nint signal_info_ptr) {
+        uint ret = _work_debug.orig_fptr(signal_info_ptr);
 
-        add_work(*(i16*)(signal_info_ptr + 0x4), *(i16*)(signal_info_ptr + 0x10));
+        add_work(*(short*)(signal_info_ptr + 0x4), *(short*)(signal_info_ptr + 0x10));
 
         return ret;
     }
 
-    private static void add_work(i16 work_id, i16 entry_id) {
+    private static void add_work(short work_id, short entry_id) {
         // Was it already executed recently?
-        for (i32 i = 0; i < rew.Count; i++) {
+        for (int i = 0; i < rew.Count; i++) {
             var o = rew[i];
             if (o.work_id == work_id && o.entry_id == entry_id) {
                 o.last_updated = 0;
@@ -122,9 +71,9 @@ public unsafe class CSRModule : FhModule {
         }
 
         // Find oldest work to replace
-        i32 max = rew[0].last_updated;
-        i32 longest_idx = 0;
-        for (i32 i = 0; i < rew.Count; i++) {
+        int max = rew[0].last_updated;
+        int longest_idx = 0;
+        for (int i = 0; i < rew.Count; i++) {
             var o = rew[i];
             if (o.last_updated > max) {
                 max = o.last_updated;
@@ -136,89 +85,90 @@ public unsafe class CSRModule : FhModule {
     }
 
     private struct DisplayObject {
-        public i16 work_id;
-        public i16 entry_id;
-        public i32 last_updated;
+        public short work_id;
+        public short entry_id;
+        public int last_updated;
     }
 
-    private const i32 REW_MAX = 42;
+    //TODO: Change whatever this was to use ImGui
+    private const int REW_MAX = 42;
     private static List<DisplayObject> rew = new(REW_MAX);
-    private static u8[] colors = new u8[] { 0xE0, 0xB0, 0xA0 };
-    public override void render_game() {
-        int x = 430;
-        int y = 5;
-
-        draw_text(0, FhCharset.Us.to_bytes("CSR is running!"), x, y, color: 0x00, 0, scale: 0.5f, 0);
-        y += 10;
-
-        if (Globals.actors != null) {
-            Actor* tidus = Globals.actors;
-            List<u8> tidus_pos = new();
-            tidus_pos.AddRange(FhCharset.Us.to_bytes($"X: {tidus->chr_pos_vec.x:f4},"));
-            tidus_pos.Add(0x2);
-            tidus_pos.AddRange(FhCharset.Us.to_bytes($"Y: {tidus->chr_pos_vec.y:f4},"));
-            tidus_pos.Add(0x2);
-            tidus_pos.AddRange(FhCharset.Us.to_bytes($"Z: {tidus->chr_pos_vec.z:f4},"));
-            draw_text(0, tidus_pos.ToArray(), x, y, color: 0x00, 0, scale: 0.5f, 0);
-            y += 30;
-        }
-
-        string event_name = Marshal.PtrToStringAnsi((isize)get_event_name(*(u32*)Globals.event_id))!;
-        draw_text(0, FhCharset.Us.to_bytes($"Event: {event_name}"), x, y, color: 0x00, 0, scale: 0.5f, 0);
-        y += 10;
-
-        draw_text(0, FhCharset.Us.to_bytes($"Map|Spawn: {Globals.save_data->current_room_id}|{Globals.save_data->current_spawnpoint}"),
-                  x, y, color: 0x00, 0, scale: 0.5f, 0);
-        y += 10;
-
-        draw_text(0, FhCharset.Us.to_bytes($"Story Progress: {Globals.save_data->story_progress}"), x, y, color: 0x00, 0, scale: 0.5f, 0);
-        y += 10;
-
-        List<u8> works = new();
-        works.AddRange(FhCharset.Us.to_bytes("Recent Signal Targets:"));
-
-        works.Add(0x2);
-
-        for (i32 i = 0; i < rew.Count; i++) {
-            var o = rew[i];
-            if (o.last_updated < 12) {
-                works.Add(0xA);
-                works.Add(colors[o.last_updated / 4]);
-            }
-
-            works.AddRange(FhCharset.Us.to_bytes($"{o.work_id:X2}:{o.entry_id:X2}"));
-
-            works.Add(0xA);
-            works.Add(0x41);
-
-            works.AddRange(FhCharset.Us.to_bytes(", "));
-
-            if (i % 3 == 2) works.Add(0x2);
-        }
-
-        draw_text(0, works.ToArray(), x, y, color: 0x00, 0, scale: 0.5f, 0);
-    }
+    // private static byte[] colors = new byte[] { 0xE0, 0xB0, 0xA0 };
+    // public override void render_game() {
+    //     int x = 430;
+    //     int y = 5;
+    //
+    //     draw_text(0, FhCharset.Us.to_bytes("CSR is running!"), x, y, color: 0x00, 0, scale: 0.5f, 0);
+    //     y += 10;
+    //
+    //     if (Globals.actors != null) {
+    //         Actor* tidus = Globals.actors;
+    //         List<byte> tidus_pos = new();
+    //         tidus_pos.AddRange(FhCharset.Us.to_bytes($"X: {tidus->chr_pos_vec.x:f4},"));
+    //         tidus_pos.Add(0x2);
+    //         tidus_pos.AddRange(FhCharset.Us.to_bytes($"Y: {tidus->chr_pos_vec.y:f4},"));
+    //         tidus_pos.Add(0x2);
+    //         tidus_pos.AddRange(FhCharset.Us.to_bytes($"Z: {tidus->chr_pos_vec.z:f4},"));
+    //         draw_text(0, tidus_pos.ToArray(), x, y, color: 0x00, 0, scale: 0.5f, 0);
+    //         y += 30;
+    //     }
+    //
+    //     string event_name = Marshal.PtrToStringAnsi((nint)get_event_name(*(uint*)Globals.event_id))!;
+    //     draw_text(0, FhCharset.Us.to_bytes($"Event: {event_name}"), x, y, color: 0x00, 0, scale: 0.5f, 0);
+    //     y += 10;
+    //
+    //     draw_text(0, FhCharset.Us.to_bytes($"Map|Spawn: {Globals.save_data->current_room_id}|{Globals.save_data->current_spawnpoint}"),
+    //               x, y, color: 0x00, 0, scale: 0.5f, 0);
+    //     y += 10;
+    //
+    //     draw_text(0, FhCharset.Us.to_bytes($"Story Progress: {Globals.save_data->story_progress}"), x, y, color: 0x00, 0, scale: 0.5f, 0);
+    //     y += 10;
+    //
+    //     List<byte> works = new();
+    //     works.AddRange(FhCharset.Us.to_bytes("Recent Signal Targets:"));
+    //
+    //     works.Add(0x2);
+    //
+    //     for (int i = 0; i < rew.Count; i++) {
+    //         var o = rew[i];
+    //         if (o.last_updated < 12) {
+    //             works.Add(0xA);
+    //             works.Add(colors[o.last_updated / 4]);
+    //         }
+    //
+    //         works.AddRange(FhCharset.Us.to_bytes($"{o.work_id:X2}:{o.entry_id:X2}"));
+    //
+    //         works.Add(0xA);
+    //         works.Add(0x41);
+    //
+    //         works.AddRange(FhCharset.Us.to_bytes(", "));
+    //
+    //         if (i % 3 == 2) works.Add(0x2);
+    //     }
+    //
+    //     draw_text(0, works.ToArray(), x, y, color: 0x00, 0, scale: 0.5f, 0);
+    // }
 
     public override void post_update() {
-        for (i32 i = 0; i < rew.Count; i++) {
+        for (int i = 0; i < rew.Count; i++) {
             var o = rew[i];
             o.last_updated++;
             rew[i] = o;
         }
     }
 
-    public void csr_event(u32 event_id) {
+    public void csr_event(uint event_id) {
         _csr_event.orig_fptr(event_id);
 
         rew.Clear();
 
-        string event_name = Marshal.PtrToStringAnsi((isize)get_event_name(event_id))!;
+        string event_name = Marshal.PtrToStringAnsi((nint)get_event_name(event_id))!;
         if (removers.TryGetValue(event_name, out CsrEvent? remover)) {
-            FhLog.Info($"Remover available for event \"{event_name}\"");
-            u8* code_ptr = Globals.Atel.controllers[0].worker(0)->code_ptr;
+            _logger.Info($"Remover available for event \"{event_name}\"! Removing cutscenes...");
+            byte* code_ptr = Globals.Atel.controllers[0].worker(0)->code_ptr;
             remover(code_ptr);
         } else {
-            FhLog.Info($"Remover not available for event \"{event_name}\"");
+            _logger.Info($"Remover not available for event \"{event_name}\".");
         }
     }
 }
--- src/removers.cs
+++ src/removers.cs
@@ -1,73 +1,68 @@
-﻿using Fahrenheit.CoreLib.FFX;
-using Fahrenheit.CoreLib.FFX.Atel;
+﻿using Fahrenheit.Core.FFX;
+using Fahrenheit.Core.Atel;
 using System.Runtime.InteropServices;
 
-using static Fahrenheit.Modules.CSR.CSRModule;
+namespace Fahrenheit.Mods.CSR;
 
-namespace Fahrenheit.Modules.CSR;
-
-internal static unsafe partial class Removers {
-    [LibraryImport("msvcrt.dll", EntryPoint = "memset", SetLastError = false)]
-    [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
-    private static partial isize memset(isize dst, i32 value, isize size);
-
+internal unsafe static partial class Removers {
     public static void init() {
-        removers.Add("znkd0600", Zanarkand.remove_intro);
-        removers.Add("znkd1000", Zanarkand.remove_prelude);
-        removers.Add("znkd1400", Zanarkand.remove_overpass);
-        removers.Add("znkd1300", Zanarkand.remove_stadium);
-        removers.Add("znkd1200", Zanarkand.remove_stadium_attacked);
+        CutsceneRemoverModule.removers.Add("znkd0600", Zanarkand.remove_intro);
+        CutsceneRemoverModule.removers.Add("znkd1000", Zanarkand.remove_prelude);
+        CutsceneRemoverModule.removers.Add("znkd1400", Zanarkand.remove_overpass);
+        CutsceneRemoverModule.removers.Add("znkd1300", Zanarkand.remove_stadium);
+        CutsceneRemoverModule.removers.Add("znkd1200", Zanarkand.remove_stadium_attacked);
 
-        removers.Add("cdsp0700", CidsShip.remove_underwater_ruins);
+        CutsceneRemoverModule.removers.Add("cdsp0700", CidsShip.remove_underwater_ruins);
 
-        removers.Add("bsil0300", Besaid.remove_valley);
-        removers.Add("bsil0600", Besaid.remove_promontory);
-        removers.Add("bsil0700", Besaid.remove_village_slope);
+        CutsceneRemoverModule.removers.Add("bsil0300", Besaid.remove_valley);
+        CutsceneRemoverModule.removers.Add("bsil0600", Besaid.remove_promontory);
+        CutsceneRemoverModule.removers.Add("bsil0700", Besaid.remove_village_slope);
     }
 
-    private static void remove(u8* code_ptr, i32 from, i32 to) {
-        memset((isize)code_ptr + from, 0, to - from);
+    private static void remove(byte* code_ptr, int from, int to) {
+        NativeMemory.Fill(code_ptr, (nuint)(to - from), 0);
     }
 
-    private static void set(u8* code_ptr, i32 offset, u8 value) {
+    private static void set(byte* code_ptr, int offset, byte value) {
         *(code_ptr + offset) = value;
     }
 
-    private static u8* set(u8* code_ptr, i32 offset, params AtelOpCode[] opcodes) {
-        u8* ptr = code_ptr + offset;
-        foreach (AtelOpCode opcode in opcodes) {
-            foreach (u8 b in opcode.to_bytes()) {
+    private static byte* set(byte* code_ptr, int offset, params AtelInst[] opcodes) {
+        byte* ptr = code_ptr + offset;
+
+        foreach (AtelInst opcode in opcodes) {
+            foreach (byte b in opcode.to_bytes()) {
                 *ptr = b;
                 ptr++;
             }
         }
+
         return ptr;
     }
 
-    private static void set_tp(u8* code_ptr, i32 offset, u16 x_idx, u16 y_idx, u16 z_idx) {
-        u8* ptr = code_ptr + offset;
-        set(ptr, 0x0, AtelInst.PUSHF.build(x_idx));
-        set(ptr, 0x3, AtelInst.PUSHF.build(y_idx));
-        set(ptr, 0x6, AtelInst.PUSHF.build(z_idx));
-        set(ptr, 0x9, AtelInst.CALLPOPA.build(0x126));
+    private static void set_tp(byte* code_ptr, int offset, ushort x_idx, ushort y_idx, ushort z_idx) {
+        byte* ptr = code_ptr + offset;
+        set(ptr, 0x0, AtelOp.PUSHF.build(x_idx));
+        set(ptr, 0x3, AtelOp.PUSHF.build(y_idx));
+        set(ptr, 0x6, AtelOp.PUSHF.build(z_idx));
+        set(ptr, 0x9, AtelOp.CALLPOPA.build(0x126));
     }
 
     private static class CidsShip {
-        public static void remove_underwater_ruins(u8* code_ptr) {
+        public static void remove_underwater_ruins(byte* code_ptr) {
             // Remove the initial cutscene of entering the ruins
             remove(code_ptr, 0x3671, 3578);
             set(code_ptr, 0x3671,
-                AtelInst.PUSHII.build(0x4C),
-                AtelInst.POPV.build(0x0)); // Set the story progress to the expected value
+                AtelOp.PUSHII.build(0x4C),
+                AtelOp.POPV.build(0x0)
+            ); // Set the story progress to the expected value
 
             remove(code_ptr, 0x36B4, 0x36BA); // Remove a fade from black
-
-
         }
     }
 
     private static class Besaid {
-        public static void remove_valley(u8* code_ptr) {
+        public static void remove_valley(byte* code_ptr) {
             remove(code_ptr, 0x1B29, 0x1C43); // Wakka pushes Tidus into the water
 
             remove(code_ptr, 0x3398, 0x33C6); // Initial fadeout into cutscene
@@ -75,17 +70,17 @@ internal static unsafe partial class Removers {
 
             // Skip the Promontory since it'd be instantly skipped anyway
             // We essentially copy bsil0600:3EB3..3EC8 to bsil0300:1FCF..1FEA
-            set(code_ptr, 0x1FCF, AtelInst.PUSHII.build(0x7E)); // GameMoment = 124 -> GameMoment = 126
-            set(code_ptr, 0x1FD5, AtelInst.PUSHII.build(0x0F)); // Common.00BB(0) -> Common.00BB(15)
-            set(code_ptr, 0x1FE1, AtelInst.PUSHII.build(69));
-            set(code_ptr, 0x1FE7, AtelInst.CALL.build(0x11)); // Common.010C(67, 0) -> Common.transitionToMap(69, 0)
+            set(code_ptr, 0x1FCF, AtelOp.PUSHII.build(0x7E)); // GameMoment = 124 -> GameMoment = 126
+            set(code_ptr, 0x1FD5, AtelOp.PUSHII.build(0x0F)); // Common.00BB(0) -> Common.00BB(15)
+            set(code_ptr, 0x1FE1, AtelOp.PUSHII.build(69));
+            set(code_ptr, 0x1FE7, AtelOp.CALL.build(0x11)); // Common.010C(67, 0) -> Common.transitionToMap(69, 0)
         }
 
-        public static void remove_promontory(u8* code_ptr) {
+        public static void remove_promontory(byte* code_ptr) {
             remove(code_ptr, 0x3DB4, 0x3EAD); // Cutscene coming from Valley DEPRECATED
         }
 
-        public static void remove_village_slope(u8* code_ptr) {
+        public static void remove_village_slope(byte* code_ptr) {
             remove(code_ptr, 0x264D, 0x289A); // First cutscene
 
             remove(code_ptr, 0x28B7, 0x28C0); // Don't make the game wait for a fade that won't happen
@@ -95,26 +90,27 @@ internal static unsafe partial class Removers {
     }
 
     private static class Zanarkand {
-        public static void remove_intro(u8* code_ptr) {
+        public static void remove_intro(byte* code_ptr) {
             // We basically write our own script here
-            set(code_ptr, 0x3DC4,
-                AtelInst.PUSHV.build(0x0),
-                AtelInst.PUSHII.build(0x2),
-                AtelInst.LS.build(),           // if (GameMoment < 2)
-                AtelInst.POPXCJMP.build(0x13), // goto intro_exit
-                                               // else
-                AtelInst.JMP.build(0xE),       // goto midgame_exit
-                AtelInst.RET.build());         // return; // not actually necessary?
-
-            // Set the story progress higher than normal to skip the prelude camera pan
-            set(code_ptr, 0x4450, AtelInst.PUSHII.build(3)); // GameMoment = 2 -> 3
-
-            // Remove some hardcoded wait time
-            remove(code_ptr, 0x43D3, 0x440A); // midgame
-            remove(code_ptr, 0x4426, 0x4450); // intro
+            // set(code_ptr, 0x3DC4,
+            //     AtelOp.PUSHV.build(0x0),
+            //     AtelOp.PUSHII.build(0x2),
+            //     AtelOp.LS.build(),           // if (GameMoment < 2)
+            //     AtelOp.POPXCJMP.build(0x13), // goto intro_exit
+            //                                  // else
+            //     AtelOp.JMP.build(0xE),       // goto midgame_exit
+            //     AtelOp.RET.build()           // return; // not actually necessary?
+            // );
+            //
+            // // Set the story progress higher than normal to skip the prelude camera pan
+            // set(code_ptr, 0x4450, AtelOp.PUSHII.build(3)); // GameMoment = 2 -> 3
+            //
+            // // Remove some hardcoded wait time
+            // remove(code_ptr, 0x43D3, 0x440A); // midgame
+            // remove(code_ptr, 0x4426, 0x4450); // intro
         }
 
-        public static void remove_prelude(u8* code_ptr) {
+        public static void remove_prelude(byte* code_ptr) {
             // The below code *works*, but it causes the game to think Tidus needs constant affirmation of his name
             // No, little child, I have a whale to sashimi
             //remove(code_ptr, 0x6A35, 0x6A9D); // Skips until running w00e10 (in w1Dtalk)
@@ -128,52 +124,54 @@ internal static unsafe partial class Removers {
             //remove(code_ptr, 0x48E, 0x54A);             // Skips until naming Tidus (in w00e12)
             //set(code_ptr, 0x550, AtelInst.RET.build()); // Skips after naming Tidus (in w00e12)
 
-            set(code_ptr, 0x6CD5, AtelInst.PUSHII.build(0x5)); // GameMoment = 4 -> 5 to skip overpass camera pan
+            set(code_ptr, 0x6CD5, AtelOp.PUSHII.build(0x5)); // GameMoment = 4 -> 5 to skip overpass camera pan
             remove(code_ptr, 0x6CDB, 0x6CF2); // Skip fade out and waits when going to next map
         }
 
-        public static void remove_overpass(u8* code_ptr) {
+        public static void remove_overpass(byte* code_ptr) {
             //TODO: Optionally skip girl's dialogue and instantly get 2 Potions
 
             // Remove some extra waiting
             remove(code_ptr, 0x292A, 0x2933);
 
             // Gotta do some manual sound stuffs
-            set(code_ptr, 0x2809, AtelInst.JMP.build(0x2)); // jump our new code
+            set(code_ptr, 0x2809, AtelOp.JMP.build(0x2)); // jump our new code
 
-            u8* ptr = code_ptr + 0x2810;
+            byte* ptr = code_ptr + 0x2810;
 
             // BGM
             ptr = set(ptr, 0,
-                AtelInst.PUSHII.build(0xA1),
-                AtelInst.CALLPOPA.build(0x102)); // call Common.setBgmToLoad(0xA1);
+                AtelOp.PUSHII.build(0xA1),
+                AtelOp.CALLPOPA.build(0x102)
+            ); // call Common.setBgmToLoad(0xA1);
 
             ptr = set(ptr, 0,
-                AtelInst.CALLPOPA.build(0x105)); // call Common.loadBgm();
+                AtelOp.CALLPOPA.build(0x105)); // call Common.loadBgm();
 
             ptr = set(ptr, 0,
-                AtelInst.PUSHII.build(0xA1),
-                AtelInst.CALLPOPA.build(0x104)); // call Common.playBgm(0xA1);
+                AtelOp.PUSHII.build(0xA1),
+                AtelOp.CALLPOPA.build(0x104)
+            ); // call Common.playBgm(0xA1);
 
             // Zanar
             ptr = set(ptr, 0,
-                AtelInst.PUSHII.build(0x1),
-                AtelInst.PUSHII.build(0x1B),
-                AtelInst.PUSHII.build(0x6),
-                AtelInst.REQ.build(),
-                AtelInst.POPA.build()); // run w1Be06 (Level 1);
+                AtelOp.PUSHII.build(0x1),
+                AtelOp.PUSHII.build(0x1B),
+                AtelOp.PUSHII.build(0x6),
+                AtelOp.REQ.build(),
+                AtelOp.POPA.build()
+            ); // run w1Be06 (Level 1);
 
-            ptr = set(ptr, 0,
-                AtelInst.JMP.build(0x3)); // go back to the game's code
+            ptr = set(ptr, 0, AtelOp.JMP.build(0x3)); // go back to the game's code
         }
 
-        public static void remove_stadium(u8* code_ptr) {
+        public static void remove_stadium(byte* code_ptr) {
             remove(code_ptr, 0x1AAF6, 0x1AED0);
 
             //TODO: Optionally remove the crowd's interruptions
         }
 
-        public static void remove_stadium_attacked(u8* code_ptr) {
+        public static void remove_stadium_attacked(byte* code_ptr) {
             // 00EF   AF0D00 AF0F00 AF0E00 D82601   call Common.warpToPoint [0126h](x=12.424045 [4146C8E3h], y=-40.966152 [C223DD57h], z=805.10956 [44494703h]);
         }
     }
